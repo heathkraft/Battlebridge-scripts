01e586920ef3d75480873ce7b490e13d4caa2b53
heathkraft <heathkraft@outlook.com>
2019-05-14 14:32:33 -0500

    comment out Ubuntu specific commands

--- deb-based-setup.sh
+++ deb-based-setup.sh
@@ -12,6 +12,7 @@ dpkg -l | grep -qw terminator || sudo apt-get install -yyq terminator
 dpkg -l | grep -qw dolphin || sudo apt-get install -yyq dolphin
 dpkg -l | grep -qw fslint || sudo apt-get install -yyq fslint
 dpkg -l | grep -qw kdeconnect || sudo apt-get install -yyq kdeconnect
+dpkg -l | grep -qw git || sudo apt-get install -yyq git
 
 # Install Chrome Browser.
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
@@ -30,34 +31,34 @@ sudo apt install ./teamviewer_amd64.deb
 rm teamviewer_amd64.deb
 
 # Install NVidia drivers, and add repo to keep them updated.
-sudo add-apt-repository ppa:graphics-drivers/ppa
-sudo ubuntu-drivers autoinstall || sudo apt upgrade -y && sudo ubuntu-drivers autoinstall
+#sudo add-apt-repository ppa:graphics-drivers/ppa
+#sudo ubuntu-drivers autoinstall || sudo apt upgrade -y && sudo ubuntu-drivers autoinstall
 
 # Install OpenRazer drivers and interface.
-sudo add-apt-repository ppa:openrazer/stable
-sudo add-apt-repository ppa:polychromatic/stable
-sudo apt update
-sudo apt install openrazer-meta -y && sudo apt install polychromatic -y
+#sudo add-apt-repository ppa:openrazer/stable
+#sudo add-apt-repository ppa:polychromatic/stable
+#sudo apt update
+#sudo apt install openrazer-meta -y && sudo apt install polychromatic -y
 
 # Install Steam client.
-sudo add-apt-repository multiverse
-sudo apt update
-sudo apt install steam
+#sudo add-apt-repository multiverse
+#sudo apt update
+#sudo apt install steam
 
 # Install Ubuntu Kernel update utility.
-sudo add-apt-repository -y ppa:teejee2008/ppa
-sudo apt update
-sudo apt install ukuu
+#sudo add-apt-repository -y ppa:teejee2008/ppa
+#sudo apt update
+#sudo apt install ukuu
 
 # Install Megasync client.
-wget https://mega.nz/linux/MEGAsync/xUbuntu_18.04/amd64/megasync-xUbuntu_18.04_amd64.deb
-sudo gdebi -n megasync-xUbuntu_18.04_amd64.deb
-rm megasync-xUbuntu_18.04_amd64.deb
+#wget https://mega.nz/linux/MEGAsync/xUbuntu_18.04/amd64/megasync-xUbuntu_18.04_amd64.deb
+#sudo gdebi -n megasync-xUbuntu_18.04_amd64.deb
+#rm megasync-xUbuntu_18.04_amd64.deb
 
 
 
-sudo add-apt-repository -y ppa:mozillateam/ppa
-sudo add-apt-repository -y ppa:libreoffice/ppa
+#sudo add-apt-repository -y ppa:mozillateam/ppa
+#sudo add-apt-repository -y ppa:libreoffice/ppa
 
 
 
