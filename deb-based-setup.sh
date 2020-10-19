42338b7e41cf34759a548450218ea6e4c26c44c0
Heath Kraft <heathkraft@outlook.com>
2020-10-19 05:26:42 -0500

    more updates

--- deb-based-setup.sh
+++ deb-based-setup.sh
@@ -45,11 +45,13 @@ dpkg -l | grep -qw htop || sudo apt install -yyq htop
 curl 'https://liquorix.net/add-liquorix-repo.sh' | sudo bash
 sudo apt install -yyq linux-image-liquorix-amd64 linux-headers-liquorix-amd64
 # Nvidia
+#dpkg -l | grep -qw htop || sudo apt install -yyq htop
 #sudo apt install -yyq nvidia-driver
 # Radeon
 # Keyboard -Ducky One2 config app
 # Mouse -Roccat NYTH config app
 # Laptop apps
+#dpkg -l | grep -qw tlp || sudo apt install -yyq tlp
 #sudo add-apt-repository ppa:webupd8team/y-ppa-manager
 #echo "deb http://ppa.launchpad.net/webupd8team/y-ppa-manager/ubuntu focal main" | sudo tee /etc/apt/sources.list.d/y-ppa-manager.list
 #sudo apt-key adv --recv-key --keyserver keyserver.ubuntu.com C2518248EEA14886
@@ -61,40 +63,45 @@ curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt
 echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 sudo apt update && sudo apt install -yyq brave-browser
 # Chrome
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo gdebi -n google-chrome-stable_current_amd64.deb
-rm google-chrome-stable_current_amd64.deb
+#wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+#sudo gdebi -n google-chrome-stable_current_amd64.deb
+#rm google-chrome-stable_current_amd64.deb
 
 # Office apps
-dpkg -l | grep -qw calibre || sudo apt install -yyq calibre
+dpkg -l | grep -qw calibre || sudo apt -t buster-backports install -yyq calibre
 echo "deb http://ppa.launchpad.net/mozillateam/ppa/ubuntu focal main" | sudo tee /etc/apt/sources.list.d/mozillateam.list
 sudo apt-key adv --recv-key --keyserver keyserver.ubuntu.com 0AB215679C571D1C8325275B9BDB3D89CE49EC21
 dpkg -l | grep -qw thunderbird || sudo apt install -yyq thunderbird
 
-echo "deb http://ppa.launchpad.net/libreoffice/libreoffice-7-0/ubuntu focal main" | sudo tee /etc/apt/sources.list.d/libreoffice7.list
-sudo apt-key adv --recv-key --keyserver keyserver.ubuntu.com 36E81C9267FD1383FCC4490983FBA1751378B444
+#echo "deb http://ppa.launchpad.net/libreoffice/libreoffice-7-0/ubuntu focal main" | sudo tee /etc/apt/sources.list.d/libreoffice7.list
+#sudo apt-key adv --recv-key --keyserver keyserver.ubuntu.com 36E81C9267FD1383FCC4490983FBA1751378B444
 
 # Remote Access Tools
 # TeamViewer
 wget https://download.teamviewer.com/download/linux/teamviewer_amd64.deb
 sudo gdebi -n teamviewer_amd64.deb
 rm teamviewer_amd64.deb
-#remmina
-#ssh
+#dpkg -l | grep -qw htop || sudo apt install -yyq remmina
+#ssh dpkg -l | grep -qw htop || sudo apt install -yyq htop
 
 # Cloud storage/sync
-sudo apt install -yyq rclone rclone-browser
-# OneDrive
-# Google Drive
-# MEGA Sync
-# Google Photos
+curl https://downloads.rclone.org/rclone-current-linux-amd64.deb
+sudo gdebi -n rclone-current-linux-amd64.deb
+rm rclone-current-current-linux-amd64.deb
+#sudo apt install -yyq rclone-browser
+# OneDrive - thru rclone
+# Google Drive - thru rclone
+# MEGA Sync - rclone or mega client
+# Google Photos - thru rclone
 
 # Project development
 dpkg -l | grep -qw freecad || sudo apt install -yyq freecad
-sudo apt install -yyq kicad 
+dpkg -l | grep -qw kicad || sudo apt -t buster-backports install -yyq kicad 
 # Python
-sudo apt install -yyq python3 python3-pip
+dpkg -l | grep -qw python3 || sudo apt install -yyq python3
+dpkg -l | grep -qw python3-pip || sudo apt install -yyq python3-pip
 # Arduino
+dpkg -l | grep -qw arduino || sudo apt -t buster-backports install -yyq arduino
 # Powershell
 wget https://packages.microsoft.com/config/debian/10/packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
@@ -105,9 +112,6 @@ sudo wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-ke
 sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
 sudo apt update && sudo apt install -yyq code
 # Notepadqq
-#sudo add-apt-repository ppa:notepadqq-team/notepadqq
-# above will only work after finding the correct signing key - installs latest ubuntu version 2.0beta
-# below works as is -installs ubuntu trusty version 1.4stable
 echo "deb http://ppa.launchpad.net/notepadqq-team/notepadqq/ubuntu trusty main" | sudo tee /etc/apt/sources.list.d/notepadqq.list
 sudo apt-key adv --recv-key --keyserver keyserver.ubuntu.com 63DE9CD4
 sudo apt update && sudo apt install -yyq notepadqq
@@ -122,8 +126,8 @@ sudo apt update && sudo apt install -yyq notepadqq
 #curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | sudo apt-key add - 
 #echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 #sudo apt update && sudo apt install -yyq spotify-client
-sudo apt install -yyq vlc
-#clementine/strawberry
+dpkg -l | grep -qw vlc || sudo apt install -yyq vlc
+#clementine/strawberry dpkg -l | grep -qw htop || sudo apt install -yyq htop
 
 # 'Graphic' design - Audio/Video editing
 # Kdenlive
@@ -137,8 +141,8 @@ sudo apt install -yyq vlc
 #sudo add-apt-repository ppa:obsproject/obs-studio
 #sudo apt update && sudo apt install -yyq obs-studio
 
-dpkg -l | grep -qw inkscape || sudo apt install -yyq inkscape
-#audacity
+dpkg -l | grep -qw inkscape || sudo apt -t buster-backports install -yyq inkscape
+dpkg -l | grep -qw audacity || sudo apt install -yyq audacity
 
 
 
@@ -146,13 +150,12 @@ dpkg -l | grep -qw inkscape || sudo apt install -yyq inkscape
 
 # Gaming
 # Steam
-#sudo apt install steam
+#sudo apt install steam-launcher dpkg -l | grep -qw htop || sudo apt install -yyq htop
 # GOG
 # Lutris
-#wget https://download.opensuse.org/repositories/home:/strycore/Debian_9.0/Release.key
-#sudo apt-key add Release.key
-#echo "deb http://download.opensuse.org/repositories/home:/strycore/Debian_9.0/ ./" | sudo tee /etc/apt/sources.list.d/lutris.list
-#sudo apt install -yyq lutris
+echo "deb http://download.opensuse.org/repositories/home:/strycore/Debian_10/ ./" | sudo tee /etc/apt/sources.list.d/lutris.list
+wget -q https://download.opensuse.org/repositories/home:/strycore/Debian_10/Release.key -O- | sudo apt-key add -
+sudo apt update && sudo apt install -yyq lutris
 # DOS Box
 
 
@@ -167,9 +170,7 @@ dpkg -l | grep -qw inkscape || sudo apt install -yyq inkscape
 
 sudo apt full-upgrade -yy
 sudo apt autoremove -yy
-
 sudo apt autoclean
-
 
 echo "-------------------"
 echo "- Setup Complete! -"
