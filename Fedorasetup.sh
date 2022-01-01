ec583505a2c5d13c72a31693acd8cdd57f8290fc
Heath Kraft <heathkraft@outlook.com>
2021-12-31 21:29:31 -0600

    some reordering and further edits

--- Fedorasetup.sh
+++ Fedorasetup.sh
@@ -9,6 +9,7 @@ sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-rele
 sudo dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 #Flathub
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+dnf check-update
 
 #cli tools
 sudo dnf -y install wget
@@ -17,6 +18,7 @@ neofetch
 #customization tools
 grub-customizer
 latte-dock
+yakuake
 
 #General usage apps [browser, email, terminal]
 #Brave browser:
@@ -26,55 +28,10 @@ sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
 sudo dnf install brave-browser
 
 thunderbird
-yakuake
-
-#Project tools
-vscode:
-sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
-sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
-dnf check-update
-sudo dnf install code
-
-notepadqq -- flatpak
-For more streamlined downloading of AppImages download AppImageUpdate (GUI or the command line version). To perform an update:
-
-Using GUI
-chmod +x ./AppImageUpdate.AppImage
-./AppImageUpdate.AppImage ./FreeCAD.AppImage
-Command line
-chmod +x ./appimageupdatetool.AppImage
-./appimageupdatetool.AppImage ./FreeCAD.AppImage
-
-#content tools
-#OBS
-sudo dnf install obs-studio
-
- kdenlive -- flatpak
-gimp
-inkscape
-audacity
-
-#gaming
-lutris
-
-Steam:
-sudo dnf install steam
-
-
-
+flatpak install flathub com.github.debauchee.barrier
+calibre
 Obsidian:
 flatpak install flathub md.obsidian.Obsidian
-
-flatpak install flathub com.github.debauchee.barrier
-
-calibre
-
-
-vscodium:
-sudo rpmkeys --import https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg
-printf "[gitlab.com_paulcarroty_vscodium_repo]\nname=download.vscodium.com\nbaseurl=https://download.vscodium.com/rpms/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg" |sudo tee -a /etc/yum.repos.d/vscodium.repo
-sudo dnf install codium
-
 ? clementine -- flatpak
 
 VLC:
@@ -97,4 +54,42 @@ sudo dnf install /path/to/downloaded/package.rpm
 
 Appimage pool
 appimage launcher
+
+#Project tools
+vscode:
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+dnf check-update
+sudo dnf install code
+
+notepadqq -- flatpak
+For more streamlined downloading of AppImages download AppImageUpdate (GUI or the command line version). To perform an update:
+
+Using GUI
+chmod +x ./AppImageUpdate.AppImage
+./AppImageUpdate.AppImage ./FreeCAD.AppImage
+Command line
+chmod +x ./appimageupdatetool.AppImage
+./appimageupdatetool.AppImage ./FreeCAD.AppImage
+
+#content tools
+#OBS
+sudo dnf install obs-studio
+
+kdenlive -- flatpak
+gimp
+inkscape
+audacity
+
+#gaming
+lutris
+
+Steam:
+sudo dnf install steam
+
+vscodium:
+sudo rpmkeys --import https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg
+printf "[gitlab.com_paulcarroty_vscodium_repo]\nname=download.vscodium.com\nbaseurl=https://download.vscodium.com/rpms/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg" |sudo tee -a /etc/yum.repos.d/vscodium.repo
+sudo dnf install codium
+
 
