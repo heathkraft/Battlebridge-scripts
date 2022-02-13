d8c9d2312e39bf3ab279deecedf56fde7e6be9fb
Heath Kraft <heathkraft@outlook.com>
2022-02-12 18:52:16 -0600

    more edits, added minigalaxy to gaming

--- Fedorasetup.sh
+++ Fedorasetup.sh
@@ -13,9 +13,10 @@ flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flat
 #Brave browser
 sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/x86_64/
 sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
-#VSCode
+#Microsoft- VSCode Powershell
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+curl https://packages.microsoft.com/config/rhel/7/prod.repo | sudo tee /etc/yum.repos.d/microsoft.repo
 #Bismuth
 sudo dnf copr enable capucho/bismuth 
 Appimage pool
@@ -27,16 +28,16 @@ dnf check-update
 sudo dnf -y install git wget dnf-plugins-core neofetch
 
 #customization tools
-sudo dnf install bismuth grub-customizer latte-dock yakuake
+sudo dnf -y install bismuth grub-customizer latte-dock yakuake
 
 
 #The usual suspects [browser, email client, media player]
 #Brave browser
-sudo dnf install brave-browser
+sudo dnf -y install brave-browser
 #Thunderbird email client
-sudo dnf install thunderbird
+sudo dnf -y install thunderbird
 #VLC and Clementine media players
-sudo dnf install vlc python-vlc
+sudo dnf -y install vlc python-vlc
 
 
 flatpak install flathub com.github.debauchee.barrier
@@ -46,7 +47,7 @@ flatpak install flathub md.obsidian.Obsidian
 ? clementine -- flatpak
 
 
-Teamviewer:
+#Teamviewer
 wget https://download.teamviewer.com/download/linux/teamviewer.x86_64.rpm
 sudo dnf -y install ./teamviewer.x86_64.rpm
 rm teamviewer.x86_64.rpm
@@ -63,7 +64,12 @@ sudo dnf install /path/to/downloaded/package.rpm
 appimage launcher
 
 #Project tools -- VSCode, Notepadqq, FreeCAD, Kicad, Arduino, SuperSlicer, xournal++, obsidian, Powershell
-sudo dnf install code xournalpp
+sudo dnf -y install code xournalpp
+#powershell
+sudo dnf install compat-openssl10
+sudo dnf install -y powershell
+#or
+#sudo dnf install https://github.com/PowerShell/PowerShell/releases/download/v7.2.1/powershell-lts-7.2.1-1.rh.x86_64.rpm
 
 notepadqq -- flatpak
 For more streamlined downloading of AppImages download AppImageUpdate (GUI or the command line version). To perform an update:
@@ -81,7 +87,7 @@ sudo dnf -y install obs-studio gimp inkscape audacity
 kdenlive -- flatpak
 
 #gaming -- Steam, Lutris
-sudo dnf -y install steam lutris
+sudo dnf -y install steam lutris minigalaxy
 
 #vscodium:
 #sudo rpmkeys --import https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg
