2614e364b0b46a70ffe38dd9850332ecaebaac7e
Heath Kraft <heathkraft@outlook.com>
2022-04-15 02:35:25 -0500

    more edits

--- Fedorasetup.sh
+++ Fedorasetup.sh
@@ -86,14 +86,8 @@ sudo dnf -y install obs-studio gimp inkscape audacity
 
 kdenlive -- flatpak
 
-#gaming -- Steam, Lutris
+#gaming -- Steam, Lutris, MiniGalaxy
 sudo dnf -y install steam lutris minigalaxy
-
-#vscodium:
-#sudo rpmkeys --import https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg
-#printf "[gitlab.com_paulcarroty_vscodium_repo]\nname=download.vscodium.com\nbaseurl=https://download.vscodium.com/rpms/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg" |sudo tee -a /etc/yum.repos.d/vscodium.repo
-#sudo dnf install codium
-
 
 echo "-------------"
 echo "- Complete! -"
