9d9cfb09098123f191bf63e81fd9ad3c26d27ebf
Heath Kraft <heathkraft@outlook.com>
2024-03-10 08:23:08 -0500

    added app install commands

--- Main-Fedora.sh
+++ Main-Fedora.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Application installation script for a fresh install
+# Setup script for a fresh battlebridge installation
 
 #improve DNF settings
 echo 'fastestmirror=True' >> /etc/dnf/dnf.conf
@@ -8,40 +8,40 @@ echo 'max_parallel_downloads=10' >> /etc/dnf/dnf.conf
 echo 'keepcache=True' >> /etc/dnf/dnf.conf
 
 # Insure everything is up to date before proceeding
-sudo dnf -y update --best --allowerasing --skip-broken
+dnf -y update --best --allowerasing --skip-broken
 
 # Add repositories
 # rpmfusion
-sudo dnf -y install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
-sudo dnf -y install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+dnf -y install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+dnf -y install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 # Flathub
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 # Brave browser
-sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/x86_64/
-sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
+dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/x86_64/
+rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
 # Microsoft
-sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
-sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
-curl https://packages.microsoft.com/config/rhel/7/prod.repo | sudo tee /etc/yum.repos.d/microsoft.repo
+rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+curl https://packages.microsoft.com/config/rhel/7/prod.repo | tee /etc/yum.repos.d/microsoft.repo
 # Enable COPRs
-sudo dnf copr enable sentry/xone
+dnf copr enable sentry/xone
 
 dnf check-update
 
 # Cli tools
-sudo dnf -y install git wget dnf-plugins-core
+dnf -y install git wget dnf-plugins-core
 # Cli toys
-sudo dnf -y install neofetch
+dnf -y install cmatrix
 
 # Appimage mgmt
-sudo mkdir /home/h/Appimages
+mkdir /home/h/Appimages
 wget https://github.com/TheAssassin/AppImageLauncher/releases/download/v2.2.0/appimagelauncher-2.2.0-travis995.0f91801.x86_64.rpm
-sudo dnf -y install ./appimagelauncher-2.2.0-travis995.0f91801.x86_64.rpm
+dnf -y install ./appimagelauncher-2.2.0-travis995.0f91801.x86_64.rpm
 rm appimagelauncher-2.2.0-travis995.0f91801.x86_64.rpm
 wget https://github.com/AppImageCommunity/AppImageUpdate/releases/download/2.0.0-alpha-1-20230526/AppImageUpdate-x86_64.AppImage -P /home/h/Appimages
 
-# My preferred apps. Browser, email, etc.
-sudo dnf -y install brave-browser thunderbird vlc python-vlc
+# The usual apps
+dnf -y install brave-browser thunderbird vlc python-vlc
 
 # Cloud services
 # NextCloud
@@ -51,24 +51,30 @@ sudo dnf -y install brave-browser thunderbird vlc python-vlc
 
 # Knowledge mgmt
 # calibre
+flatpak install flathub com.calibre_ebook.calibre
 # obsidian
+flatpak install flathub md.obsidian.Obsidian
 
 # Cross-platform mgmt
 # barrier/whatever supports wayland
 # Teamviewer, until I find better
 wget https://download.teamviewer.com/download/linux/teamviewer.x86_64.rpm
-sudo dnf -y install ./teamviewer.x86_64.rpm
+dnf -y install ./teamviewer.x86_64.rpm
 rm teamviewer.x86_64.rpm
 
 # Comms
 # discord
+flatpak install flathub com.discordapp.Discord
 # signal
 
 # Project tools
 
 # Content creation
+flatpak install flathub com.obsproject.Studio
 
 # Gaming
+flatpak install flathub com.heroicgameslauncher.hgl
+flatpak install flathub net.lutris.Lutris
 
 echo "-------------"
 echo "- Complete! -"
